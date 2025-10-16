e0bb9707d2c7ebe35238ba5e8b828722ef59957c
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-10-16 23:17:27 +0500

    Add todo state management to App component

--- 2. Todo App/src/App.jsx
+++ 2. Todo App/src/App.jsx
@@ -1,8 +1,39 @@
-import './App.css'
 import TodoNav from './components/TodoNav'
+import './App.css'
+
 
 function App() {
-	return <TodoNav/>;
+	/*
+	let todos =[
+		{itemName: "Learn React",dueDate:"31-12-2025"},
+		{itemName: "Learn Node",dueDate:"31-12-2025"},
+		{itemName: "Learn MongoDB",dueDate:"31-12-2025"},
+		{itemName: "Learn Express",dueDate:"31-12-2025"},
+		{itemName:"Have Biryani",dueDate:"31-10-2025"}
+	];
+	*/
+	const [todos,setTodos] = useState([]);
+
+	const addNewTodo =(newTodoItem,newTodoDueDate)=>{
+		console.log(`{newTodoItem}  added with due date {newTodoDueDate}`);
+
+		setTodos((currentItems)=>{
+			return [...currentItems,{itemName:newTodoItem,dueDate:newTodoDueDate}]
+		})
+	}
+
+	const deleteTodo =(itemName)=>{
+		console.log(`{itemName} deleted`);
+
+		const newItems = todos.filter((item)=>{	item.itemName !== itemName	})
+
+		setTodos(newItems);
+	}
+
+	return <>
+		<TodoNav></TodoNav>
+	</>
+	
 
 }
 
