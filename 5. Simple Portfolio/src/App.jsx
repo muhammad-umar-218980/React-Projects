b992e5beb8814909dd6a4e79e5fc5415dfe057d8
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-10-18 00:48:11 +0500

    Integrate portfolio sections into App component

--- 5. Simple Portfolio/src/App.jsx
+++ 5. Simple Portfolio/src/App.jsx
@@ -1,9 +1,33 @@
-function App() {
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Projects';
+import Certifications from './components/Certifications/Certifications';
+import Coursework from './components/Coursework/Coursework';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import BackToTop from './components/BackToTop/BackToTop';
 
+import './App.css';
+
+function App() {
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Certifications />
+        <Coursework />
+        <Contact />
+      </main>
+      <Footer />
+      <BackToTop />
+    </div>
+  );
 }
 
-export default App
+export default App;
