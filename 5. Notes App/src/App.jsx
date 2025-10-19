b339206449b78c7f7fea6b75223017444f87ef74
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-10-20 01:32:51 +0500

    Added Navbar component with css

--- 5. Notes App/src/App.jsx
+++ 5. Notes App/src/App.jsx
@@ -2,13 +2,11 @@ import Navbar from './components/Navbar/Navbar.jsx'
 import './App.css'
 
 
-let App = function(){
+let App = function () {
 
   return (<>
-    <h1 className='bg-amber-400'>Hello Tailwind Css</h1>
-    <Navbar></Navbar>
+    <Navbar />
   </>)
 }
 
 export default App;
-
