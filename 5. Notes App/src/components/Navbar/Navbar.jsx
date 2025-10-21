1981378ad45016d4a2148dbe974be8ca171b7703
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-10-21 21:02:56 +0500

    Removed fixed class from Navbar

--- 5. Notes App/src/components/Navbar/Navbar.jsx
+++ 5. Notes App/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 w-full py-10 bg-black/90 backdrop-blur-sm shadow-lg z-50">
+    <nav className="top-0 w-full py-10 bg-black/90 backdrop-blur-sm shadow-lg z-50">
       <div className="flex justify-center items-center">
         <h1 className={`text-3xl font-bold text-white`}>
           My Notes App 📝
